73d99758bee469392add01b06e96f08aa9426597
pan-shile <3502173090@qq.com>
2026-06-22 23:23:14 +0800

    变量和运算符

---
+++ test2.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+    "fmt"
+)
+
+
+func main() {
+	var age int
+	var name = "张三"
+
+	age = 100	
+	fmt.Printf("%d",age)
+	fmt.Printf("\n")
+	fmt.Printf("%s",name)
+	age++
+	fmt.Printf("%d",age)
+}
+
